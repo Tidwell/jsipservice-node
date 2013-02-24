cb47577eb72eb6facbbb9ef016a4a6913a34e555
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-02-24 18:20:51 -0500

    nav active clas

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -36,3 +36,10 @@ DocsCtrl.$inject = [];
 function ContactCtrl() {
 }
 ContactCtrl.$inject = [];
+
+function NavCtrl($scope,$location) {
+	$scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1) || 'home';
+        return page === currentRoute ? 'active' : '';
+    };
+}
